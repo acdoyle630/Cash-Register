be98dc8c6a3358ae14a55dd32f557ea3f0394a9f
Adam <acdoyle630@gmail.com>
2017-03-18 09:59:22 -1000

    dollas

--- js/cash_register.js
+++ js/cash_register.js
@@ -6,6 +6,8 @@ function clear() {
 	var clearDis = document.getElementById('display');
 	clearDis.innerHTML = '';
 	displayParent.appendChild(clearDis);
+	console.log(newDis);
+	console.log(typeof newDis);
 }
 
 function reset(){
@@ -14,6 +16,8 @@ function reset(){
 	calcTest.clearMemory();
 	dotAllow = false;
 	buttonOrder = [];
+	console.log(newDis);
+	console.log(typeof newDis);
 }
 
 function showBalance(){
@@ -21,15 +25,20 @@ function showBalance(){
 	displayBal.innerHTML = balance.toFixed(2);
 	displayParent.appendChild(displayBal);
 	newDis = Number(balance);
-
+	console.log(newDis);
+	console.log(typeof newDis);
 }
 
 function withdraw(x) {
 	if (x <= balance) {
 		balance -= x;
 		reset();
+		console.log(newDis);
+		console.log(typeof newDis);
 	} else {
 	alert('You broke as fuck! Current Balance: $' + balance.toFixed(2));
+	console.log(newDis);
+	console.log(typeof newDis);
 	}
 }
 
@@ -38,13 +47,17 @@ function runningDisplay(num) {
 	changeDisplay.innerHTML = num.toFixed(2);
 	displayParent.appendChild(changeDisplay);
 	newDis = Number(changeDisplay.innerHTML);
+	console.log(newDis);
+	console.log(typeof newDis);
 }
 
 function updateDisplay (num) {
 	var grabDisplay = document.getElementById('display');
 	grabDisplay.innerHTML += num;
-	displayParent.appendChild(grabDisplay);
 	newDis = Number(grabDisplay.innerHTML);
+	displayParent.appendChild(grabDisplay);
+	console.log(newDis);
+	console.log(typeof newDis);
 }
 
 function addDec (num) {
@@ -52,6 +65,8 @@ function addDec (num) {
 	decGrab.innerHTML = newDis + "." + num;
 	displayParent.appendChild(decGrab);
 	newDis = Number(decGrab.innerHTML);
+	console.log(newDis);
+	console.log(typeof newDis);
 }
 
 function resetDisplay (){
@@ -59,10 +74,14 @@ function resetDisplay (){
 	zeroDisplay.innerHTML = '';
 	displayParent.appendChild(zeroDisplay);
 	newDis = zeroDisplay.innerHTML;
+	console.log(newDis);
+	console.log(typeof newDis);
 }
 
 function addButton(str) {
 	buttonOrder.push(str);
+	console.log(newDis);
+	console.log(typeof newDis);
 }
 
 
@@ -70,131 +89,193 @@ var buttonOrder = [];
 
 document.getElementById('one').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length - 1] !== 'equals' && buttonOrder[buttonOrder.length - 1] !== 'dot'){
-	updateDisplay(1);	
+	updateDisplay(1);
+	console.log(newDis);
+	console.log(typeof newDis);	
 	}
 	else if (dotAllow === false && buttonOrder[buttonOrder.length - 1] !== 'equals') {
 	updateDisplay(1);
+	console.log(newDis);
+	console.log(typeof newDis);
 	}
 	else if(dotAllow === true){
 		addDec(1);
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
 document.getElementById('two').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length-1] !== 'equals' && buttonOrder[buttonOrder.length - 1] !== 'dot'){
 	updateDisplay(2);
+	console.log(newDis);
+	console.log(typeof newDis);
 	}
 	else if (dotAllow === false && buttonOrder[buttonOrder.length - 1] !== 'equals') {
 	updateDisplay(2);
+	console.log(newDis);
+	console.log(typeof newDis);
 	}
 	if(dotAllow === true){
 		addDec(2);
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
 document.getElementById('three').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length-1] !== 'equals' && buttonOrder[buttonOrder.length - 1] !== 'dot'){
 	updateDisplay(3);
+	console.log(newDis);
+	console.log(typeof newDis);
 	}
 	else if (dotAllow === false && buttonOrder[buttonOrder.length - 1] !== 'equals') {
 	updateDisplay(3);
+	console.log(newDis);
+	console.log(typeof newDis);
 	}
 	if(dotAllow === true){
 		addDec(3);
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
 document.getElementById('four').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length-1] !== 'equals' && buttonOrder[buttonOrder.length - 1] !== 'dot'){
 	updateDisplay(4);
+	console.log(newDis);
+	console.log(typeof newDis);
 	}
 	else if (dotAllow === false && buttonOrder[buttonOrder.length - 1] !== 'equals') {
 	updateDisplay(4);
+	console.log(newDis);
+	console.log(typeof newDis);
 	}
 	if(dotAllow === true){
 		addDec(4);
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
 document.getElementById('five').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length-1] !== 'equals' && buttonOrder[buttonOrder.length - 1] !== 'dot'){
 	updateDisplay(5);
+	console.log(newDis);
+	console.log(typeof newDis);
 	}
 	else if (dotAllow === false && buttonOrder[buttonOrder.length - 1] !== 'equals') {
 	updateDisplay(5);
+	console.log(newDis);
+	console.log(typeof newDis);
+
 	}
 	if(dotAllow === true){
 		addDec(5);
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
+
 	}
 });
 
 document.getElementById('six').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length - 1] !== 'equals' && buttonOrder[buttonOrder.length - 1] !== 'dot'){
 	updateDisplay(6);
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 	else if (dotAllow === false && buttonOrder[buttonOrder.length - 1] !== 'equals') {
 	updateDisplay(6);
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 	if(dotAllow === true){
 		addDec(6);
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
 document.getElementById('seven').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length - 1] !== 'equals' && buttonOrder[buttonOrder.length - 1] !== 'dot'){
 	updateDisplay(7);
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 	else if (dotAllow === false && buttonOrder[buttonOrder.length - 1] !== 'equals') {
 	updateDisplay(7);
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 	if(dotAllow === true){
 		addDec(7);
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
 document.getElementById('eight').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length - 1] !== 'equals' && buttonOrder[buttonOrder.length - 1] !== 'dot'){
 	updateDisplay(8);
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 	else if (dotAllow === false && buttonOrder[buttonOrder.length - 1] !== 'equals') {
 	updateDisplay(8);
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 	if(dotAllow === true){
 		addDec(8);
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
 document.getElementById('nine').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length - 1] !== 'equals' && buttonOrder[buttonOrder.length - 1] !== 'dot'){
 	updateDisplay(9);
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 	else if (dotAllow === false && buttonOrder[buttonOrder.length - 1] !== 'equals') {
 	updateDisplay(9);
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 	if(dotAllow === true){
 		addDec(9);
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
 document.getElementById('zero').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length - 1] !== 'equals' && buttonOrder[buttonOrder.length - 1] !== 'dot'){
 	updateDisplay(0);
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 	else if (dotAllow === false && buttonOrder[buttonOrder.length - 1] !== 'equals') {
 	updateDisplay(0);
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 	if(dotAllow === true){
 		addDec(0);
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
@@ -202,17 +283,22 @@ document.getElementById('dot').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length - 1] !== 'equals'){
 	if(buttonOrder.indexOf('dot') === - 1){
 		dotAllow = true;
+		console.log(newDis);
+		console.log(typeof newDis);
 	} 
 	addButton('dot');
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
 document.getElementById('doubleZero').addEventListener('click', function(){
 	dotAllow = !dotAllow;
-	//if(dotAllow === true){
 		if (buttonOrder.indexOf('dot') === -1) {
 		addDec('00');
 		dotAllow = false;
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 
 });
@@ -222,6 +308,8 @@ document.getElementById('plus').addEventListener('click', function(){
 	resetDisplay();
 	buttonOrder.splice(buttonOrder.indexOf('dot'),1);
 	addButton('plus');
+	console.log(newDis);
+		console.log(typeof newDis);
 	} 
 );
 
@@ -230,6 +318,8 @@ document.getElementById('minus').addEventListener('click', function(){
 	resetDisplay();
 	buttonOrder.splice(buttonOrder.indexOf('dot'),1);
 	addButton('minus');
+	console.log(newDis);
+		console.log(typeof newDis);
 	} 
 );
 
@@ -238,6 +328,8 @@ document.getElementById('times').addEventListener('click', function(){
 	resetDisplay();
 	buttonOrder.splice(buttonOrder.indexOf('dot'),1);
 	addButton('times');
+	console.log(newDis);
+		console.log(typeof newDis);
 	} 
 );
 
@@ -246,6 +338,8 @@ document.getElementById('divide').addEventListener('click', function(){
 	resetDisplay();
 	buttonOrder.splice(buttonOrder.indexOf('dot'),1);
 	addButton('divide');
+	console.log(newDis);
+		console.log(typeof newDis);
 	} 
 );
 
@@ -254,18 +348,30 @@ document.getElementById('equals').addEventListener('click', function(){
 		var total = calcTest.add(newDis);
 		runningDisplay(total);
 		addButton('equals');
+		console.log(newDis);
+		console.log(typeof newDis);
 	} 
 	if(buttonOrder[buttonOrder.length - 1] === 'minus' || buttonOrder[buttonOrder.length - 1] === 'dot' && buttonOrder[buttonOrder.length - 2] === 'minus') {
 		var subTotal = calcTest.subtract(newDis);
 		runningDisplay(subTotal);
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 	if(buttonOrder[buttonOrder.length-1] === 'times' || buttonOrder[buttonOrder.length - 1] === 'dot' && buttonOrder[buttonOrder.length - 2] === 'times') {
 		var mulTotal = calcTest.multiply(newDis);
 		runningDisplay(mulTotal);
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 	if(buttonOrder[buttonOrder.length - 1] === 'divide' || buttonOrder[buttonOrder.length - 1] === 'dot' && buttonOrder[buttonOrder.length - 2] === 'divide') {
+		if(newDis !==0){
 		var divTotal = calcTest.divide(newDis);
 		runningDisplay(divTotal);
+	} else {
+		alert('cannot divide by zero');
+	}
+		console.log(newDis);
+		console.log(typeof newDis);
 	}
 	buttonOrder = ['equals'];
 });
@@ -273,12 +379,16 @@ document.getElementById('equals').addEventListener('click', function(){
 document.getElementById('clear').addEventListener('click', function(){
 	clear();
 	buttonOrder.push('clear');
+	console.log(newDis);
+		console.log(typeof newDis);
 });
 
 document.getElementById('reset').addEventListener('click', function(){
 	reset();
 	newDis = 0;
 	buttonOrder.push('reset');
+	console.log(newDis);
+		console.log(typeof newDis);
 });
 
 document.getElementById('deposit').addEventListener('click', function(){
@@ -286,18 +396,24 @@ document.getElementById('deposit').addEventListener('click', function(){
 	balance += Number(newDis);
 	reset();
 	buttonOrder.push('deposit');
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
 document.getElementById('balance').addEventListener('click', function(){
 	showBalance();
 	buttonOrder.push('balance', 'equals');
+	console.log(newDis);
+		console.log(typeof newDis);
 });
 
 document.getElementById('withdraw').addEventListener('click', function(){
 	if(buttonOrder[buttonOrder.length - 2] !== 'balance' && Number(newDis) > 0) {
 	withdraw(Number(newDis));
 	buttonOrder.push('withdraw');
+	console.log(newDis);
+		console.log(typeof newDis);
 	}
 });
 
